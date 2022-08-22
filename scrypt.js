26d6ffc168dc3332353cc40757dc444af604bf3e
viacheslav Kurov <0990004793qw@gmail.com>
2022-08-22 15:26:14 +0300

    completed work 10

---
+++ scrypt.js
@@ -0,0 +1,26 @@
+"use strict";
+class Employee {
+  vacationDays = 18;
+  constructor(firstName, lastName, age, jobPosition, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = Number(age);
+    this.jobPosition = jobPosition;
+    this.salary = Number(salary);
+  }
+  get fullName() {
+    console.log(`Full name: ${this.firstName} ${this.lastName}`);
+  }
+  get yearSalarOfEmployee() {
+    console.log(`Year salary: ${this.salary * this.vacationDays}`);
+  }
+}
+
+const EmployeeInfo = new Employee("Dandy", "Bibik", 20, "Juniorloader", 500);
+const EmployeeOfStock = new Employee("Garry", "Fisher", 25, "Supervisor", 1000);
+console.log(EmployeeInfo);
+console.log(EmployeeOfStock);
+EmployeeInfo.fullName;
+EmployeeInfo.yearSalarOfEmployee;
+EmployeeOfStock.fullName;
+EmployeeOfStock.yearSalarOfEmployee;
